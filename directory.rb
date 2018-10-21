adc0cb9ce5d2e7fadc7bbdcb18af639ae3d4a8b7
leoncross <leon@lscross.co.uk>
2018-10-21 17:17:38 +0100

    put code into methods

--- directory.rb
+++ directory.rb
@@ -12,12 +12,18 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
-
-puts "The students of Villains Academy"
-puts "--------------"
-
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "--------------"
 end
-
-print "Overall, we have #{students.count} great students"
+def print_names(names)
+  names.each do |student|
+    puts student
+  end
+end
+def print_footer(names)
+  print "Overall, we have #{names.count} great students"
+end
+print_header
+print_names(students)
+print_footer(students)
